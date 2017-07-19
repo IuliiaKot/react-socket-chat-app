84aba3c476fa07df7edae37d5f104a64bc31353c
IuliiaKot <kotlenko.julia@gmail.com>
2017-07-19 17:39:06 -0400

    display chat only when user enter username

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@ class App extends React.Component {
     this.state = {
       messages: [],
       userMessage: '',
-      hideLogin: false
+      displaUserLogin: true
     }
     // this.socket = io(`http://localhost:3000`)
     this.handleChange = this.handleChange.bind(this);
-    this.joinToChat = this.joinToChat.bind(this);
+    this.userInput = this.userInput.bind(this);
   }
 
   componentDidMount(){
@@ -43,62 +43,43 @@ class App extends React.Component {
     this.socket.emit('createMessage', message)
     this.setState({userMessage: ''})
   }
-
-  joinToChat(e){
-    // debugger
-    let test = document.getElementsByClassName('login')
-    if (e.which === 13) {
-        this.setState({hideLogin: true})
-      //  this.socket.emit('createMessage', {name: "Adam", text:"welcome"})
+  userInput(e){
+    if (e.which == 13) {
+      this.setState({displaUserLogin: false})
     }
   }
 
   render(){
     return(
-     
-        <ul className="pages">
-            <li className="chat page">
-                <aside className='left'>
-                    <div className='rooms-list'>
-                      <h4>Rooms</h4>
-                      <div className="list-group col-md-12" id="rooms">
-                        <Rooms/>
-                      </div>
-                    </div>
-                    <div className='users-list'>
-                      <h4>Users</h4>
-                      <div className="list-group col-md-12" id="users">
-                      </div>
-                    </div>
-
-                </aside>
-                <aside className='right'>
-                  <div className="chat">
-                    <div className="chat-history panel-body body-panel">
-                      <ul className="chat-ul">
-                      </ul>
-                    </div>
-                  </div>
-                </aside>
-                <div>
-                <Form id="form" onSubmit={this.handleSubmit.bind(this)}> 
-                <Input size="lg" type="text" name="message" placeholder="with a placeholder" 
-                  value={this.state.userMessage}
-                  onChange={this.handleChange}/>
-              </Form> 
-              <MessagesList messages={this.state.messages}/>
-              </div> 
-            </li>  
-           {this.state.hideLogin ? null :   
-          <li className="login page">
+      <div>
+      {
+        this.state.displaUserLogin ?
+          <span className="pages">
+          <div className="login page">
             <div className="form">
               <h3 className="title">What's your nickname?</h3>
-              <input className="usernameInput" type="text" maxLength="14" onKeyPress={this.joinToChat}/>
+              <input className="usernameInput" type="text" maxLength="14" 
+              onKeyPress={this.userInput}/>
             </div>
-          </li>
-           }
-        </ul>    
+          </div>
+        </span>   :
 
+ 
+      <Container>
+        <Row>
+          <Col md="3"><Rooms/></Col>
+          <Col md="9">
+           <Form id="form" onSubmit={this.handleSubmit.bind(this)}> 
+            <Input size="lg" type="text" name="message" placeholder="with a placeholder" 
+              value={this.state.userMessage}
+              onChange={this.handleChange}/>
+           </Form>     
+            <MessagesList messages={this.state.messages}/>
+          </Col>
+        </Row> 
+      </Container>
+     }
+      </div>
     )
   }
 }
