d6cd8f8e383a3c83fedbdf424b3e30bdd075a833
IuliiaKot <kotlenko.julia@gmail.com>
2017-08-04 11:59:49 -0400

    add scroll

--- src/components/MessagesList.js
+++ src/components/MessagesList.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import Message from './Message';
 import PropTypes from 'prop-types';
 
-const MessagesList = (props) => {
+class MessagesList extends Component {
+  scrollToBottom() {
+    const node = ReactDOM.findDOMNode(this.messagesEnd);
+    node.scrollIntoView({ behavior: "smooth" });
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+  render(){
   return (
       <div className="chat">
         <div class="chat-history panel-body body-panel">
           <ul className="chat-ul">
             {
-              props.messages.map( (message,idx) => {
+              this.props.messages.map( (message,idx) => {
                 return <Message message={message} key={idx}/>;
               })
             }
           </ul>
         </div>
-        {        window.scrollBy(0, 1000)}
+        <div style={{ float:"left", clear: "both" }}
+             ref={(el) => { this.messagesEnd = el; }} />
       </div>
   );
+  };
 };
 
 MessagesList.propTypes = {
